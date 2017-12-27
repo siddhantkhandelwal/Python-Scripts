9f1f071ba0b380f5db89f9b73e9e3d6ef03e6509
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-12-27 13:34:15 +0530

    Changed storage format to JSON

--- Logger/main.py
+++ Logger/main.py
@@ -1,26 +1,51 @@
 import sys
+import re
+import json
 from os import path, makedirs
 from datetime import datetime
 
 timestamp = datetime.fromtimestamp
-TODAY = datetime.now().date()
-
-PROMPT = sys.argv[1][:-1].title() + ">"
-FILEPATH = path.join(path.expanduser('~'), 'LOGS', sys.argv[1])
-MODE = 'a'
-
-if (not path.exists(FILEPATH)):
-    if not path.exists(path.dirname(FILEPATH)):
-        makedirs(path.dirname(FILEPATH))
-    MODE = 'w'
-
-if(MODE == 'w' or timestamp((path.getmtime(FILEPATH))).date() != TODAY):
-    with open(FILEPATH, MODE) as f:
-        f.write("\n##" + str(TODAY) + ":\n")
+TODAY = datetime.now().strftime("%B%d")
 try:
+    PROMPT = sys.argv[1][:-1].title() + ">"
+except IndexError:
+    sys.exit("Missing input!")
+
+FILEPATH = path.join(path.expanduser('~'), 'LOGS', sys.argv[1]) + ".json"
+CLOSE_ERR = (EOFError, KeyboardInterrupt)
+
+if(__name__=="__main__"):
+    if (not path.exists(FILEPATH)):
+        if not path.exists(path.dirname(FILEPATH)):
+            makedirs(path.dirname(FILEPATH))
+
+    try:
+        with open(FILEPATH,'r') as f:
+            data = json.load(f)
+            try:
+                data = data[sys.argv[1]]
+            except KeyError:
+                data = {}
+    except (IOError):    
+        with open(FILEPATH,'w') as f:
+            json.dump({sys.argv[1]:{}},f)
+        data = {}
+
+    if (not(TODAY in data)):
+        today_list = []
+    else:
+        today_list = data[TODAY]
+
     while True:
-        note = raw_input(PROMPT)
-        with open(FILEPATH, 'a') as f:
-            f.write(note + "\n")
-except KeyboardInterrupt:
-    sys.exit()
+        try:
+            note = input(PROMPT)
+            if (re.fullmatch('(\\x1b\[[ABCD])*',note)):
+                continue
+            today_list.append(note)
+
+        except CLOSE_ERR:
+            sys.exit()
+        with open(FILEPATH,'w') as f:
+            data.update({TODAY: today_list})
+            json.dump({sys.argv[1]:data},f)
+            datetime.now().strftime("%B%d")
