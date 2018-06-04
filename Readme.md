daff7640ea0086a893bc53c54338ede57edde91c
siddhantkhandelwal <siddhant@outlook.com>
2018-06-05 00:37:25 +0530

    Scheduled alarms implemented. Readme.md Refactored

--- Readme.md
+++ Readme.md
@@ -1,40 +1,57 @@
 # Python Scripts
-This repository contains all of my Python scripts made for simplifying tasks.
+**Repository containing Python scripts made for simplifying common tasks.** :sunglasses:
 
-### Libraries Used:
+## Libraries Used:
  - os
  - sys
  - openpyxl
  - hashlib
  - filecmp
  - struct
- - Crypto (in auto-backup)
- - shutil (in auto-backup)
+ - Crypto           (in auto-backup)
+ - shutil           (in auto-backup)
+ - vlc              (in alarm-clock)
+ - mutagen          (in alarm-clock)
+ - python-crontab   (in alarm-clock)
 
-### List of Scripts:
-1. Duplicate: 
+## Scripts:
+1. [Duplicate](https://github.com/Pro-Panda/Python-Scripts/tree/master/Duplicate): 
    - It lists all the duplicate files present at a given path.
    - Gives the option to remove/ not remove any of them.
    - Uses 'filecmp' and 'os' libraries to compare files which are of the same size
-   - How to use:
-      ```bash
-      python filecmp.py /path/to/folder
-      ```
-2. Excel-Attd: 
+   
+2. [Excel-Attd](https://github.com/Pro-Panda/Python-Scripts/tree/master/Excel-Attd): 
    - Fills the attendance of all BITS Students(mathced by ID) in an excel sheet.
    - Created to fill the attendance of Students registered in CP Course at BITS Pilani
-   - How to use:
-      ```bash
-      python update.py /path/to/attendance_list /path/to/excel_sheet <Attd Column No.>
-      ```
-3. Alarm-Clock:
+   
+3. [Alarm-Clock](https://github.com/siddhantkhandelwal/Python-Scripts/tree/master/alarm-clock):
    - Plays the file 'alarm.mp3' in the cwd once the alarm expires.
-   - Will be adding other featues, like snooze, ring daily/weekly
-   - How to use:
-      ```bash
-      python alarm-clock-cli.py h m s
-      ```
+   - Can be used to schedule alarms weekly, daily.
+   
+## How to use:
+ - Duplicate
 
-### Improvement:
-Anyone is welcome to contribute and improve/modify the scripts. <br>
-Please open an issue if you find any bugs in any of the scripts.
+   ```bash
+   python filecmp.py /path/to/folder
+   ```
+ - Excel-Attd
+
+    ```bash
+    python update.py /path/to/attendance_list /path/to/excel_sheet <Attd Column No.>
+    ```
+ - Alarm-Clock
+
+    ```bash
+    python alarm-clock-cli.py option h m s
+    ```
+
+## Improvements:
+*Anyone is welcome to contribute and improve/modify the scripts.*
+
+- [Alarm-Clock](https://github.com/siddhantkhandelwal/Python-Scripts/tree/master/alarm-clock):
+  - Add an option to snooze the alarm.
+  - Add an option to delete/modify scheduled alarms.
+  - Add an option for custom alarm sounds.
+  - Create logs specific to the script. Will help in keeping track of alarms independent of the cron-tab.
+
+*Please open an issue if you find any bugs in any of the scripts.*
--- alarm-clock/alarm-clock-cli.py
+++ alarm-clock/alarm-clock-cli.py
@@ -1,45 +1,64 @@
 '''
 Requirements:
-vlc:        pip install vlc
-mutagen:    pip install mutagen
+vlc:            pip install vlc
+mutagen:        pip install mutagen
+python-crontab: pip install python-crontab
 '''
 
 import sys
 import string
 import vlc
+import os
+import getpass
 from time import sleep
 from mutagen.mp3 import MP3
+from crontab import CronTab
+
 
 if len(sys.argv) < 2 or len(sys.argv) > 4:
-    print("Usage: [ python ] alarm-clock-cli.py h m s")
-    print("Example: [ python ] alarm-clock-cli.py 10 25 56")
-    print("Example: [ python ] alarm-clock-cli.py 0 4 20")
+    print("\nUsage: [ python ] alarm-clock-cli.py [option] h m")
+    print("Example: [ python ] alarm-clock-cli.py 1 10 25")
+    print("Example: [ python ] alarm-clock-cli.py 1 0 4")
+    print("Example: [ python ] alarm-clock-cli.py 2")
     print("Use a value of 0 for testing the alarm immediately.")
+    print("Example: [ python ] alarm-clock-cli.py 0")
     print("Plays mp3(only) file stored in cwd named as 'alarm.mp3'")
     print("Press Ctrl-C to terminate the alarm clock early.")
     sys.exit(1)
 
-total_seconds = 0
+username = getpass.getuser()
+script_path = os.path.abspath(sys.argv[0])
 
-if sys.argv[1] == 0 and len(sys.argv) == 2:
-    total_seconds = 0
-elif len(sys.argv)==4:
-    hours = int(sys.argv[1])
-    minutes = int(sys.argv[2])
-    seconds = int(sys.argv[3])
-    total_seconds = hours*60*60 + minutes*60 + seconds
-elif len(sys.argv)==3:
-    hours = int(sys.argv[1])
-    minutes = int(sys.argv[2])
-    total_seconds = hours*60*60 + minutes*60
-else:
-    hours = int(sys.argv[1])
-    total_seconds = hours*60*60
+if int(sys.argv[1]) == 0:
+    print(os.path.dirname(script_path))
+    alarm_file = vlc.MediaPlayer(os.path.join(os.path.dirname(script_path), "alarm.mp3"))
+    alarm_file_length = MP3(os.path.join(os.path.dirname(script_path), "alarm.mp3")).info.length
+    alarm_file.play()
+    slep(ealarm_file_length)
 
-print("Sleeping for %d seconds" % total_seconds)
-alarm_file = vlc.MediaPlayer("alarm.mp3")
-alarm_file_length = MP3("alarm.mp3").info.length
-sleep(total_seconds)
-alarm_file.play()
-#print(audio.info.length)
-sleep(alarm_file_length)
+elif int(sys.argv[1]) == 1:
+    hours= int(sys.argv[2])
+    minutes = int(sys.argv[3])
+    
+    print("Do yo want to schedule the alarm to ring daily/weekly? ('d' for daily, 'w' for weekly): ")
+    schedule_period = input()
+    
+    if schedule_period == 'd':
+        cron_task = CronTab(user=username)
+        job = cron_task.new(command='python ' + script_path + ' 0')
+        job.hour.on(hours)
+        job.minute.on(minutes)
+        job.day.every(1)
+        print(job.is_valid())
+        cron_task.write()
+        
+    elif schedule_period == 'w':
+        print("Which day of the week? (0-6): ")
+        dow = int(input())
+        cron_task = CronTab(user=username)
+        job = my_cron.new(command="python '" + script_path + "' 0")
+        job.hour.on(hours)
+        job.minute.on(minutes)
+        job.dow.on(dow)
+        job.week.every(1)
+        cron_task.write()
